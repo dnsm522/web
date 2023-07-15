97730886c01e4ef88bb58d187804d6d8b326d6cf
dnsm522 <dnsm522@naver.com>
2023-07-16 02:22:08 +0900

    모

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){  
 
-    var 신생높이 =  $("#신생").outerHeight();
-    var 창천높이 = 신생높이 + $("#창천").outerHeight();
+    var 신생높이 =  $("#신생").outerHeight(true);
+    var 창천높이 = (신생높이 + $("#창천").outerHeight(true));
+    var 홍련높이 = (창천높이 + $("#홍련").outerHeight(true));
     // console.log(신생높이);
     console.log(창천높이);
         // 신생높이 = 1637
     $(window).scroll(function(){
         var scrollheight = $(window).scrollTop()
             console.log(scrollheight);
-            if(신생높이 -400 <= scrollheight){
+            if(신생높이 -1500 <= scrollheight){
                 $(".신생1").addClass("ani");
                 
                 setTimeout(function(){
                     $(".신생2").addClass("ani");
                 },1000);
             }
-            if(창천높이 +1200 <= scrollheight){
+            if(창천높이 -1000 <= scrollheight){
                 $(".창천1").addClass("ani");
 
                 setTimeout(function(){
