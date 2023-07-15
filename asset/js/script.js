933ae4ebf71554fe3ae4533caefbf134a3d47e8c
dnsm522 <dnsm522@naver.com>
2023-07-16 02:28:37 +0900

    모

--- asset/js/script.js
+++ asset/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     var 창천높이 = (신생높이 + $("#창천").outerHeight(true));
     var 홍련높이 = (창천높이 + $("#홍련").outerHeight(true));
     // console.log(신생높이);
-    console.log(창천높이);
+    //console.log(창천높이);
+    console.log(홍련높이); //홍련높이 5450
         // 신생높이 = 1637
     $(window).scroll(function(){
         var scrollheight = $(window).scrollTop()
@@ -27,9 +28,15 @@ $(document).ready(function(){
                     $(".창천3").addClass("ani");
                 },2000);
             }
-            
-            
+            if(홍련높이 -1000 <= scrollheight){
+                $(".홍련1").addClass("ani");
 
+                setTimeout(function(){
+                    $(".홍련2").addClass("ani");
+                },1000);           
+            }
+
+            
     })
    
 
